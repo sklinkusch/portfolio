8b232a647b8aba5fe0fbff5658d2774a327e3564
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-10 20:08:16 +0200

    imported titleimage

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/App.scss";
 import MyNavbar from "./MyNavbar";
+import TitleImage from "./TitleImage";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
+      <TitleImage />
     </div>
   );
 }
---
+++ src/Components/TitleImage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TitleImage() {
+  return (
+    <div className="sk-img-container sk-w100vw sk-h60vw sk-flex-col sk-jc-end sk-ai-end">
+      <div className="sk-small-img">
+        <img src="./Stefan.jpg" alt="Dr. Stefan Klinkusch" />
+      </div>
+    </div>
+  );
+}
