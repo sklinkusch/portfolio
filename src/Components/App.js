e88e9fd606f8546973060337075456adc7e72f39
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-10 21:04:53 +0200

    added jumbotron

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/App.scss";
 import MyNavbar from "./MyNavbar";
 import TitleImage from "./TitleImage";
+import Welcome from "./Welcome";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
       <TitleImage />
+      <Welcome />
     </div>
   );
 }
