4c5146d565c88b37b32635c892519486c5c24058
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-11 13:39:58 +0200

    added CSS

--- src/Components/Compskills.js
+++ src/Components/Compskills.js
@@ -11,6 +11,11 @@ export default class Compskills extends React.Component {
           name: "HTML5",
           imageSrc: "images/HTML5_logo_and_wordmark.svg",
           imageId: "html"
+        },
+        {
+          name: "CSS3",
+          imageSrc: "images/CSS.3.svg.png",
+          imageId: "css"
         }
       ]
     };
@@ -21,9 +26,11 @@ export default class Compskills extends React.Component {
         <div className="sk-placeholder-tiny" />
         <section id="compskills">
           <h3> Computer-related Skills</h3>
-          {this.state.skills.map((skill, index) => (
-            <CompskillCard skill={skill} key={index} />
-          ))}
+          <div className="row album">
+            {this.state.skills.map((skill, index) => (
+              <CompskillCard skill={skill} key={index} />
+            ))}
+          </div>
         </section>
         <div className="sk-placeholder-tiny" />
       </div>
