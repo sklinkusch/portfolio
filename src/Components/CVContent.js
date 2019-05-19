8ec5c7681542378d85c0e6a973d1586151deb5aa
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-19 16:12:44 +0200

    fixed typography in CV

--- src/Components/CVContent.js
+++ src/Components/CVContent.js
@@ -7,72 +7,112 @@ export default function CVContent(props) {
     {
       list: [
         {
-          time: "1989-1995",
-          desc: "Primary school in Berlin, Germany"
+          time: <span>1989-1995</span>,
+          desc: <span>Primary school in Berlin, Germany</span>
         },
         {
-          time: "1995-2002",
-          desc: "Grammar school in Berlin, Germany"
+          time: <span>1995-2002</span>,
+          desc: <span>Grammar school in Berlin, Germany</span>
         },
         {
-          time: "2002-2008",
-          desc: "Studies of chemistry at University of Potsdam, Germany"
+          time: <span>2002-2008</span>,
+          desc: (
+            <span>Studies of chemistry at University of Potsdam, Germany</span>
+          )
         },
         {
-          time: "2007-2008",
-          desc:
-            "Diploma thesis in the group of Prof. P. Saalfrank at University of Potsdam, Germany"
+          time: <span>2007-2008</span>,
+          desc: (
+            <span>
+              Diploma thesis in the group of Prof. P. Saalfrank at University of
+              Potsdam, Germany
+            </span>
+          )
         },
         {
-          time: "2008-2011",
-          desc:
-            "Ph.D thesis in the group of Prof. P. Saalfrank at University of Potsdam, Germany"
+          time: <span>2008-2011</span>,
+          desc: (
+            <span>
+              Ph.D thesis in the group of Prof. P. Saalfrank at University of
+              Potsdam, Germany
+            </span>
+          )
         },
         {
-          time: "2016",
-          desc: "Educating class at Works gGmbH in Berlin, Germany"
+          time: <span>2016</span>,
+          desc: <span>Educating class at Works gGmbH in Berlin, Germany</span>
         },
         {
-          time: "since 2018",
-          desc:
-            "Web development class at Digital Career Institute in Berlin, Germany"
+          time: <span>since 2018</span>,
+          desc: (
+            <span>
+              Web development class at Digital Career Institute in Berlin,
+              Germany
+            </span>
+          )
         }
       ]
     },
     {
       list: [
         {
-          time: "2002",
-          desc:
-            "University entry exam (Abitur), mark 1.5 (1.0 best, 4,0 just passed, 6.0 worst)"
+          time: <span>2002</span>,
+          desc: (
+            <span>
+              University entry exam (<span className="italic">Abitur</span>),
+              mark 1.5 (1.0 best, 4,0 just passed, 6.0 worst)
+            </span>
+          )
         },
         {
-          time: "2008",
-          desc:
-            "Diploma in chemistry, mark 1.8 (1.0 best, 4.0 just passed, 5.0 worst)"
+          time: <span>2008</span>,
+          desc: (
+            <span>
+              Diploma in chemistry, mark 1.8 (1.0 best, 4.0 just passed, 5.0
+              worst)
+            </span>
+          )
         },
         {
-          time: "2011",
-          desc: "Ph.D in science (Dr. rer. nat.) mark magna cum laude"
+          time: <span>2011</span>,
+          desc: (
+            <span>
+              Ph.D in science (<span className="italic">Dr. rer. nat.</span>)
+              mark <span className="italic">magna cum laude</span>
+            </span>
+          )
         },
         {
-          time: "2016",
-          desc:
-            "Educator's qualification certificate according to the German Order for the educator's qualification (Ausbilder-Eignungsverordnung, AEVO)"
+          time: <span>2016</span>,
+          desc: (
+            <span>
+              Educator's qualification certificate according to the German Order
+              for the educator's qualification (
+              <abbr title="Ausbilder-Eignungsverordnung">AEVO</abbr>)
+            </span>
+          )
         }
       ]
     },
     {
       list: [
         {
-          time: "2008-2011",
-          desc:
-            "Scientific coworker in the group of Prof. P. Saalfrank at University of Potsdam, Germany"
+          time: <span>2008-2011</span>,
+          desc: (
+            <span>
+              Scientific coworker in the group of Prof. P. Saalfrank at
+              University of Potsdam, Germany
+            </span>
+          )
         },
         {
-          time: "2014-2016",
-          desc:
-            "Scientific coworker in the group of Dr. J. C. Tremblay at Free University of Berlin, Germany"
+          time: <span>2014-2016</span>,
+          desc: (
+            <span>
+              Scientific coworker in the group of Dr. J. C. Tremblay at Free
+              University of Berlin, Germany
+            </span>
+          )
         }
       ]
     }
