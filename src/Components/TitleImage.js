42e26750b4b872a15476671d4f23020eda0dc26f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-02 15:47:45 +0200

    loaded my image from src

--- src/Components/TitleImage.js
+++ src/Components/TitleImage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import stefan from "../images/Stefan.jpg";
 
 export default function TitleImage() {
   return (
     <div className="sk-img-container sk-w100vw sk-h60vw sk-flex-col sk-jc-end sk-ai-end">
       <div className="sk-small-img">
-        <img src="./Stefan.jpg" alt="Dr. Stefan Klinkusch" />
+        <img src={stefan} alt="Dr. Stefan Klinkusch" />
       </div>
     </div>
   );
