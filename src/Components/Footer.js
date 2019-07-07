c6e5880cde4273c46b6df8005709bc85c6eeddc0
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-07 09:52:30 +0200

    added xing logo, map over elements

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,38 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Footer.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 export default function Footer() {
+  const [links] = useState([
+    {
+      href: "https://github.com/sklinkusch",
+      class: "github",
+      icon: "fab fa-github-square"
+    },
+    {
+      href: "https://www.linkedin.com/in/stefan-klinkusch",
+      class: "linkedin",
+      icon: "fab fa-linkedin"
+    },
+    {
+      href: "https://xing.to/StefanKlinkusch",
+      class: "xing",
+      icon: "fab fa-xing-square"
+    },
+    {
+      href: "mailto:stefan.klinkusch@googlemail.com",
+      class: "mail",
+      icon: "fas fa-envelope"
+    }
+  ]);
   return (
     <div className="container sk-bg-white">
       <footer>
         <div className="links">
-          <a
-            href="https://github.com/sklinkusch/"
-            className="github"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github-square" />
-          </a>
-          <span>{"  "}</span>
-          <a
-            href="https://www.linkedin.com/in/stefan-klinkusch-b0595216a"
-            className="linkedin"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-linkedin" />
-          </a>
-          <span>{"  "}</span>
-          <a
-            href="mailto:stefan.klinkusch@googlemail.com"
-            className="mail"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fas fa-envelope" />
-          </a>
+          {links
+            .map((link, index) => (
+              <a
+                href={link.href}
+                className={link.class}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={index}
+              >
+                <i className={link.icon} />
+              </a>
+            ))
+            .reduce((prev, curr) => [prev, "  ", curr])}
         </div>
         <p className="copyright">&copy; Stefan Klinkusch, 2019</p>
       </footer>
