a6b40923418c0f83c33dcbea2338a03f15b24da1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-10 17:39:19 +0200

    refactor Footer

--- src/Context/AppProvider.js
+++ src/Context/AppProvider.js
@@ -906,6 +906,28 @@ export default class AppProvider extends Component {
           href: "https://sklinkusch.github.io/VBB",
           src: "https://github.com/sklinkusch/VBB/raw/master/Screenshot.png"
         }
+      ],
+      footerLinks: [
+        {
+          href: "https://github.com/sklinkusch",
+          class: "github",
+          icon: "fab fa-github-square"
+        },
+        {
+          href: "https://www.linkedin.com/in/stefan-klinkusch",
+          class: "linkedin",
+          icon: "fab fa-linkedin"
+        },
+        {
+          href: "https://xing.to/StefanKlinkusch",
+          class: "xing",
+          icon: "fab fa-xing-square"
+        },
+        {
+          href: "mailto:stefan.klinkusch@googlemail.com",
+          class: "mail",
+          icon: "fas fa-envelope"
+        }
       ]
     };
   }
