311e1d47c5fbd7f10c528d5fae20d55e470ddb97
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-10 11:32:24 +0200

    refactor Compskills

--- src/Components/Compskills.js
+++ src/Components/Compskills.js
@@ -1,167 +1,27 @@
 import React from "react";
+import AppContext from "../Context/AppContext";
 import CompskillCard from "./CompskillCard";
 import "../styles/Compskills.scss";
-import html from "../images/HTML5_logo_and_wordmark.svg";
-import css from "../images/CSS.3.svg.png";
-import sass from "../images/Sass_Logo_Color.svg";
-import git from "../images/Git-logo.svg";
-import bootstrap from "../images/Boostrap_logo.svg";
-import javascript from "../images/ES6LOGO.svg";
-import react from "../images/React.svg";
-import redux from "../images/redux-logo.png";
-import graphql from "../images/480px-GraphQL_Logo.svg.png";
-import nodejs from "../images/Node.js_logo.svg";
-import express from "../images/Expressjs.png";
-import mongodb from "../images/MongoDB.png";
-import cplusplus from "../images/ISO_C++_Logo.svg";
-import fortran from "../images/f90-300x212.png";
-import perl from "../images/Perl.png";
-import bash from "../images/Bash.svg";
-import latex from "../images/220px-LaTeX_logo.svg.png";
-import vscode from "../images/Visual_Studio_Code_1.35_icon.svg";
-import brackets from "../images/Brackets.png";
-import linux from "../images/Linux.svg";
-import ubuntu from "../images/Ubuntu.svg";
-import opensuse from "../images/OpenSUSE.svg";
-import slack from "../images/Slack.svg";
 
-export default class Compskills extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      skills: [
-        {
-          name: "HTML5",
-          imageSrc: html,
-          imageId: "html"
-        },
-        {
-          name: "CSS3",
-          imageSrc: css,
-          imageId: "css"
-        },
-        {
-          name: "Sass/SCSS",
-          imageSrc: sass,
-          imageId: "sass"
-        },
-        {
-          name: "git",
-          imageSrc: git,
-          imageId: "git"
-        },
-        {
-          name: "Bootstrap 4",
-          imageSrc: bootstrap,
-          imageId: "bootstrap"
-        },
-        {
-          name: "ECMAScript 6",
-          imageSrc: javascript,
-          imageId: "ecmascript"
-        },
-        {
-          name: "React",
-          imageSrc: react,
-          imageId: "react"
-        },
-        {
-          name: "Redux",
-          imageSrc: redux,
-          imageId: "redux"
-        },
-        {
-          name: "GraphQL",
-          imageSrc: graphql,
-          imageId: "graphql"
-        },
-        {
-          name: "Node.js",
-          imageSrc: nodejs,
-          imageId: "nodejs"
-        },
-        {
-          name: "Express.js",
-          imageSrc: express,
-          imageId: "express"
-        },
-        {
-          name: "MongoDB",
-          imageSrc: mongodb,
-          imageId: "mongodb"
-        },
-        {
-          name: "C++",
-          imageSrc: cplusplus,
-          imageId: "cplusplus"
-        },
-        {
-          name: "Fortran 90",
-          imageSrc: fortran,
-          imageId: "fortran"
-        },
-        {
-          name: "Perl",
-          imageSrc: perl,
-          imageId: "perl"
-        },
-        {
-          name: "Bourne-Again Shell",
-          imageSrc: bash,
-          imageId: "bash"
-        },
-        {
-          name: "LaTeX",
-          imageSrc: latex,
-          imageId: "latex"
-        },
-        {
-          name: "VS Code",
-          imageSrc: vscode,
-          imageId: "vscode"
-        },
-        {
-          name: "Brackets",
-          imageSrc: brackets,
-          imageId: "brackets"
-        },
-        {
-          name: "Linux",
-          imageSrc: linux,
-          imageId: "linux"
-        },
-        {
-          name: "Ubuntu",
-          imageSrc: ubuntu,
-          imageId: "ubuntu"
-        },
-        {
-          name: "openSUSE",
-          imageSrc: opensuse,
-          imageId: "opensuse"
-        },
-        {
-          name: "Slack",
-          imageSrc: slack,
-          imageId: "slack"
-        }
-      ]
-    };
-  }
-  render() {
-    return (
-      <div className="container sk-bg-white">
-        <div className="sk-placeholder-tiny" />
-        <section id="compskills">
-          <h3> Computer-related Skills</h3>
-          <div className="row album">
-            {this.state.skills.map((skill, index) => (
-              <CompskillCard skill={skill} key={index} />
-            ))}
-          </div>
-        </section>
-        <div className="sk-placeholder-tiny" />
-      </div>
-    );
-  }
-}
+const Compskills = () => {
+  return (
+    <div className="container sk-bg-white">
+      <div className="sk-placeholder-tiny" />
+      <section id="compskills">
+        <h3> Computer-related Skills</h3>
+        <div className="row album">
+          <AppContext.Consumer>
+            {context =>
+              context.skills.map((skill, index) => (
+                <CompskillCard skill={skill} key={index} />
+              ))
+            }
+          </AppContext.Consumer>
+        </div>
+      </section>
+      <div className="sk-placeholder-tiny" />
+    </div>
+  );
+};
+
+export default Compskills;
