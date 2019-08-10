bc73a82e65def0fc884b27700c421a7352e12b6b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-10 16:49:46 +0200

    refactor Talks

--- src/Components/Talks.js
+++ src/Components/Talks.js
@@ -1,268 +1,38 @@
 import React from "react";
 import { TabPane } from "reactstrap";
+import AppContext from "../Context/AppContext";
 
-export default function Talks() {
+const Talks = () => {
   const number = 3;
   const space = " ";
-  const talks = [
-    {
-      title: (
-        <q className="arttitle">
-          Laser-driven electron dynamics in molecules using time-dependent
-          configuration interaction methods
-        </q>
-      ),
-      article: null,
-      place: <span>Bundesinstitut für Risikobewertung, Berlin, Germany</span>,
-      date: (
-        <span>
-          April 22<sup>nd</sup>, 2018
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          Time-resolved photoelectron spectra using explicitly time-dependent{" "}
-          <abbr title="configuration interaction">CI</abbr> varitaions
-        </q>
-      ),
-      article: null,
-      place: (
-        <span>Presentation Domino, Free University of Berlin, Germany</span>
-      ),
-      date: (
-        <span>
-          January 16<sup>th</sup>, 2016
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          Laser-driven electron dynamics in molecules using time-dependent
-          configuration interaction methods
-        </q>
-      ),
-      article: null,
-      place: (
-        <span>
-          Theoretical Chemistry Group Seminar, Humboldt University of Berlin,
-          Germany
-        </span>
-      ),
-      date: (
-        <span>
-          October 11<sup>th</sup>, 2012
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          Lasergetriebene Vielelektronendynamik in molekularen Systemen
-        </q>
-      ),
-      article: null,
-      place: <span>Ph.D disputation, University of Potsdam, Germany</span>,
-      date: (
-        <span>
-          October 20<sup>th</sup>, 2011
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          Electron Dynamics: photoionization, dissipation, and nuclear modes
-        </q>
-      ),
-      article: null,
-      place: (
-        <span>
-          Theoretical Chemistry Group Seminar, University of Potsdam, Germany
-        </span>
-      ),
-      date: (
-        <span>
-          June 30<sup>th</sup>, 2010
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          A heuristic model describing effects of photoionization as an
-          extension to time-dependent configuration interaction methods
-        </q>
-      ),
-      article: null,
-      place: <span>Tag der Chemie, Free University of Berlin, Germany</span>,
-      date: (
-        <span>
-          June 16<sup>th</sup>, 2010
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          A rough model describing effects of photoionization as an extension to{" "}
-          <abbr title="time-dependent configuration interaction singles">
-            TD-CIS
-          </abbr>
-        </q>
-      ),
-      article: null,
-      place: (
-        <span>
-          Theoretical Chemistry Group Seminar, University of Potsdam, Germany
-        </span>
-      ),
-      date: (
-        <span>
-          May 6<sup>th</sup>, 2009
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          First-order simultaneous optimization of molecular geometry and
-          electronic wave function
-        </q>
-      ),
-      article: (
-        <span>
-          (<span className="authors">Moss and Li</span>,{" "}
-          <span className="jtitle">The Journal of Chemical Physics</span>{" "}
-          <span className="jedition">129</span>,{" "}
-          <span className="jpages">114102</span>{" "}
-          <span className="pubyear">2008</span>)
-        </span>
-      ),
-      place: (
-        <span>
-          Journal Club, Theoretical Chemistry Group Seminar, University of
-          Potsdam, Germany
-        </span>
-      ),
-      date: (
-        <span>
-          November 5<sup>th</sup>, 2008
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          Pseudospectral time-dependent density functional theory
-        </q>
-      ),
-      article: (
-        <span>
-          (<span className="authors">Ko et al.</span>,{" "}
-          <span className="jtitle">The Journal of Chemical Physics</span>{" "}
-          <span className="jedition">128</span>,{" "}
-          <span className="jpages">104103</span>{" "}
-          <span className="pubyear">2008</span>)
-        </span>
-      ),
-      place: (
-        <span>
-          Journal Club, Theoretical Chemistry Group Seminar, University of
-          Potsdam, Germany
-        </span>
-      ),
-      date: (
-        <span>
-          May 28<sup>th</sup>, 2008
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          Theorie des femtosekundenlaserinduzierten intermolekularen
-          Elektronentransfers mittels zeitabhängiger
-          Konfigurationswechselwirkungs-Methoden
-        </q>
-      ),
-      article: null,
-      place: (
-        <span>
-          Diploma Candidates' Talks, Institute of Chemistry Seminar, University
-          of Potsdam
-        </span>
-      ),
-      date: (
-        <span>
-          May 7<sup>th</sup>, 2008
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          Laser-induced Intermolecular Electron Transfer by using{" "}
-          <abbr title="time-dependent configuration interaction singles">
-            TD-CIS
-          </abbr>
-        </q>
-      ),
-      article: null,
-      place: (
-        <span>
-          Theoretical Chemistry Group Seminar, University of Potsdam, Germany
-        </span>
-      ),
-      date: (
-        <span>
-          January 28<sup>th</sup>, 2008
-        </span>
-      )
-    },
-    {
-      title: (
-        <q className="arttitle">
-          Preparation and Excitation of Wave Packets for Cs@Cu (111)
-        </q>
-      ),
-      article: null,
-      place: (
-        <span>
-          Theoretical Chemistry Group Seminar, University of Potsdam, Germany
-        </span>
-      ),
-      date: (
-        <span>
-          March 8<sup>th</sup>, 2007
-        </span>
-      )
-    }
-  ];
   return (
     <TabPane tabId={number}>
       <ol reversed>
-        {talks.map((talk, index) => {
-          const { title, article, place, date } = talk;
-          return (
-            <li key={index}>
-              <span className="authors">Stefan Klinkusch</span>,{space}
-              {title}
-              {article !== null && (
-                <>
+        <AppContext.Consumer>
+          {context =>
+            context.talks.map((talk, index) => {
+              const { title, article, place, date } = talk;
+              return (
+                <li key={index}>
+                  <span className="authors">Stefan Klinkusch</span>,{space}
+                  {title}
+                  {article !== null && (
+                    <>
+                      {space}
+                      {article},
+                    </>
+                  )}
                   {space}
-                  {article},
-                </>
-              )}
-              {space}
-              {place},{space}
-              {date}
-            </li>
-          );
-        })}
+                  {place},{space}
+                  {date}
+                </li>
+              );
+            })
+          }
+        </AppContext.Consumer>
       </ol>
     </TabPane>
   );
-}
+};
+
+export default Talks;
