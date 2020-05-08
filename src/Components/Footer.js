22fcde46f29708a1e5c73e821f42cf28de043ecd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-08 10:23:23 +0200

    adjust copyright and year

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,31 +3,38 @@ import "../styles/Footer.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 import AppContext from "../Context/AppContext";
 
-const Footer = () => (
-  <div className="container sk-bg-white">
-    <footer>
-      <div className="links">
-        <AppContext.Consumer>
-          {context =>
-            context.footerLinks
-              .map((link, index) => (
-                <a
-                  href={link.href}
-                  className={link.class}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  key={index}
-                >
-                  <i className={link.icon} />
-                </a>
-              ))
-              .reduce((prev, curr) => [prev, "  ", curr])
-          }
-        </AppContext.Consumer>
-      </div>
-      <p className="copyright">&copy; Stefan Klinkusch, 2019</p>
-    </footer>
-  </div>
-);
+const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <div className="container sk-bg-white">
+      <footer>
+        <div className="links">
+          <AppContext.Consumer>
+            {(context) =>
+              context.footerLinks
+                .map((link, index) => (
+                  <a
+                    href={link.href}
+                    className={link.class}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={index}
+                  >
+                    <i className={link.icon} />
+                  </a>
+                ))
+                .reduce((prev, curr) => [prev, "  ", curr])
+            }
+          </AppContext.Consumer>
+        </div>
+        <p className="copyright">
+          <span>&copy;</span>
+          <span>{` Stefan Klinkusch, 2019-${year}`}</span>
+        </p>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
