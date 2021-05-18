90ae8bf7b341f6a519c695ffdd00b2702b2f9a90
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-05-18 19:49:07 +0200

    add theme-ui in compskills

--- src/Context/Skills/Styling.js
+++ src/Context/Skills/Styling.js
@@ -1,6 +1,7 @@
 import css from "../../images/CSS.3.svg.png";
 import sass from "../../images/Sass_Logo_Color.svg";
 import bootstrap from "../../images/Boostrap_logo.svg";
+import themeUi from "../../images/theme-ui.png";
 
 const styling = {
   title: "Styling",
@@ -19,6 +20,11 @@ const styling = {
       name: "Bootstrap 4",
       imageSrc: bootstrap,
       imageId: "bootstrap",
+    },
+    {
+      name: "Theme UI",
+      imageSrc: themeUi,
+      imageId: "themeUi"
     },
   ],
 };
