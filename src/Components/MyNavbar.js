c749d7626dbb3fbdf9e51d18b1a38f3b99a8b073
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-05-28 15:11:36 +0200

    use react-headroom and make navbar sticky on scroll

--- src/Components/MyNavbar.js
+++ src/Components/MyNavbar.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import Headroom from "react-headroom"
 import {
   Collapse,
   Navbar,
@@ -10,55 +11,64 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
-} from "reactstrap";
+  DropdownItem,
+} from "reactstrap"
 
 const MyNavbar = () => {
-  const [isOpen, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false)
   const toggle = () => {
-    setOpen(!isOpen);
-  };
+    setOpen(!isOpen)
+  }
   return (
-    <Navbar color="light" light expand="lg">
-      <NavbarBrand href="#">Dr. Stefan Klinkusch</NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="./index.html" className="active">
-              Home
-            </NavLink>
-          </NavItem>
-          <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
-              Skills
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem href="#compskills">
-                Computer-related skills
-              </DropdownItem>
-              <DropdownItem href="#langskills">Language skills</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-          <NavItem>
-            <NavLink href="#cv">CV</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#pubs">Publications</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#refs">References</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#examples">Examples</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#contact">Contact</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
-  );
-};
+    <Headroom
+      style={{
+        transition: "all 0.5s ease-in-out",
+        WebkitTransition: "all 0.5s ease-in-out",
+        MozTransition: "all 0.5s ease-in-out",
+        OTransition: "all 0.5s ease-in-out",
+      }}
+    >
+      <Navbar color="light" light expand="lg">
+        <NavbarBrand href="#">Dr. Stefan Klinkusch</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="./index.html" className="active">
+                Home
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Skills
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem href="#compskills">
+                  Computer-related skills
+                </DropdownItem>
+                <DropdownItem href="#langskills">Language skills</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink href="#cv">CV</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#pubs">Publications</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#refs">References</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#examples">Examples</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#contact">Contact</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </Headroom>
+  )
+}
 
-export default MyNavbar;
+export default MyNavbar
