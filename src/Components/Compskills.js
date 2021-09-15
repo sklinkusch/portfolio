eaf6b34445f34341f0d3b900df5189510dd3896f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-09-15 15:18:38 +0200

    add accordion for mobile computer-related skills

--- src/Components/Compskills.js
+++ src/Components/Compskills.js
@@ -1,30 +1,84 @@
-import React from "react";
-import AppContext from "../Context/AppContext";
-import CompskillCard from "./CompskillCard";
-import "../styles/Compskills.scss";
+import React, { useState } from "react"
+import AppContext from "../Context/AppContext"
+import CompskillCard from "./CompskillCard"
+import Accordion from "react-bootstrap/Accordion"
+// import Button from "react-bootstrap/Button"
+import "../styles/Compskills.scss"
+import { useAccordionToggle } from "react-bootstrap"
+import { FaChevronDown, FaChevronUp } from "react-icons/fa"
 
-const Compskills = () => (
-  <div className="container sk-bg-white">
-    <div className="sk-placeholder-tiny" />
-    <section id="compskills">
+const CustomToggle = ({ children, eventKey, activeKey }) => {
+  const onClickHandler = useAccordionToggle(eventKey, () => {
+    // console.log("Totally custom")
+  })
+  return (
+    <div className="sk-flex sk-row sk-jc-sb sk-ai-c">
+      <h4 onClick={onClickHandler}>{children}</h4>
+      {eventKey === activeKey ? <FaChevronUp /> : <FaChevronDown />}
+    </div>
+  )
+}
+
+const CompskillsMediumLarge = () => (
+  <section id="compskills" className="d-none d-md-block">
+    <h3> Computer-related Skills</h3>
+    <AppContext.Consumer>
+      {(context) =>
+        context.skills.map((skill, index) => (
+          <article key={index}>
+            <h4>{skill.title}</h4>
+            <div className="row album">
+              {skill.content.map((singleSkill, index) => (
+                <CompskillCard skill={singleSkill} key={index} />
+              ))}
+            </div>
+          </article>
+        ))
+      }
+    </AppContext.Consumer>
+  </section>
+)
+
+const CompskillsSmall = () => {
+  const [activeKey, setActiveKey] = useState(null)
+  return (
+    <Accordion
+      id="compskills"
+      className="d-block d-md-none"
+      activeKey={activeKey}
+      onSelect={(e) => setActiveKey(e)}
+    >
       <h3> Computer-related Skills</h3>
       <AppContext.Consumer>
         {(context) =>
-          context.skills.map((skill, index) => (
-            <article key={index}>
-              <h4>{skill.title}</h4>
-              <div className="row album">
-                {skill.content.map((singleSkill, index) => (
-                  <CompskillCard skill={singleSkill} key={index} />
-                ))}
-              </div>
+          context.skills.map((skill, oindex) => (
+            <article key={oindex}>
+              <CustomToggle eventKey={`${oindex}`} activeKey={activeKey}>
+                {/* <h4> */}
+                {skill.title} ({skill.content.length}){/* </h4> */}
+              </CustomToggle>
+              <Accordion.Collapse eventKey={`${oindex}`}>
+                <div className="row album">
+                  {skill.content.map((singleSkill, index) => (
+                    <CompskillCard skill={singleSkill} key={index} />
+                  ))}
+                </div>
+              </Accordion.Collapse>
             </article>
           ))
         }
       </AppContext.Consumer>
-    </section>
+    </Accordion>
+  )
+}
+
+const Compskills = () => (
+  <div className="container sk-bg-white">
+    <div className="sk-placeholder-tiny" />
+    <CompskillsMediumLarge />
+    <CompskillsSmall />
     <div className="sk-placeholder-tiny" />
   </div>
-);
+)
 
-export default Compskills;
+export default Compskills
