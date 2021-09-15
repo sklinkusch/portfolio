981e24b65c085621624b0023e24b7bfbfd5c306a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-09-15 10:55:37 +0200

    add some margin

--- src/Components/CompskillCard.js
+++ src/Components/CompskillCard.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from "react"
 
-const CompskillCard = props => {
-  const { imageSrc, name, imageId } = props.skill;
+const CompskillCard = (props) => {
+  const { imageSrc, name, imageId } = props.skill
   return (
-    <div className="col-xs-6 col-md-4 col-lg-3">
+    <div className="col-xs-6 col-md-4 col-lg-3 sk-mb-8px">
       <div className="album-item sk-flex-col sk-jc-spb sk-ai-c">
         <img src={imageSrc} alt={name} className="img-thumbnail" id={imageId} />
         <p>{name}</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CompskillCard;
+export default CompskillCard
