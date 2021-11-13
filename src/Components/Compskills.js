7a680d7e50b5cc1217bbcf75b9008f5f223071b9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-13 22:23:45 +0100

    switch react-bootstrap accordion to details-summary

--- src/Components/Compskills.js
+++ src/Components/Compskills.js
@@ -1,23 +1,7 @@
-import React, { useState } from "react"
+import React from "react"
 import AppContext from "../Context/AppContext"
 import CompskillCard from "./CompskillCard"
-import Accordion from "react-bootstrap/Accordion"
-// import Button from "react-bootstrap/Button"
 import "../styles/Compskills.scss"
-import { useAccordionToggle } from "react-bootstrap"
-import { FaChevronDown, FaChevronUp } from "react-icons/fa"
-
-const CustomToggle = ({ children, eventKey, activeKey }) => {
-  const onClickHandler = useAccordionToggle(eventKey, () => {
-    // console.log("Totally custom")
-  })
-  return (
-    <div className="sk-flex sk-row sk-jc-sb sk-ai-c">
-      <h4 onClick={onClickHandler}>{children}</h4>
-      {eventKey === activeKey ? <FaChevronUp /> : <FaChevronDown />}
-    </div>
-  )
-}
 
 const CompskillsMediumLarge = () => (
   <section id="compskills" className="d-none d-md-block">
@@ -40,35 +24,30 @@ const CompskillsMediumLarge = () => (
 )
 
 const CompskillsSmall = () => {
-  const [activeKey, setActiveKey] = useState(null)
   return (
-    <Accordion
+    <section
       id="compskills"
       className="d-block d-md-none"
-      activeKey={activeKey}
-      onSelect={(e) => setActiveKey(e)}
     >
       <h3> Computer-related Skills</h3>
       <AppContext.Consumer>
         {(context) =>
           context.skills.map((skill, oindex) => (
-            <article key={oindex}>
-              <CustomToggle eventKey={`${oindex}`} activeKey={activeKey}>
+            <details key={oindex}>
+              <summary>
                 {/* <h4> */}
                 {skill.title} ({skill.content.length}){/* </h4> */}
-              </CustomToggle>
-              <Accordion.Collapse eventKey={`${oindex}`}>
-                <div className="row album">
-                  {skill.content.map((singleSkill, index) => (
-                    <CompskillCard skill={singleSkill} key={index} />
-                  ))}
-                </div>
-              </Accordion.Collapse>
-            </article>
+              </summary>
+              <div className="row album">
+                {skill.content.map((singleSkill, index) => (
+                  <CompskillCard skill={singleSkill} key={index} />
+                ))}
+              </div>
+            </details>
           ))
         }
       </AppContext.Consumer>
-    </Accordion>
+    </section>
   )
 }
 
