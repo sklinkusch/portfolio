51bdc23e4641baa7a01373915d32f39dd33eb754
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-03 11:17:17 +0200

    change FormElement to typescript

--- src/Components/FormElement.tsx
+++ src/Components/FormElement.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-const FormElement = props => (
+type Props = {
+  id: string,
+  label: string,
+  type: string,
+  name: string,
+  placeholder: string,
+  fieldRef: React.Ref<HTMLInputElement>
+}
+
+const FormElement = (props: Props) => (
   <div className="form-group">
     <label htmlFor={props.id}>{props.label}</label>
     <input
