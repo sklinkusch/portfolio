58bb9bb1dc83f8ecaf5793ff7f77383521a2ed15
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-03 17:11:21 +0200

    change PubTitles to typescript

--- src/Components/PubTitles.tsx
+++ src/Components/PubTitles.tsx
@@ -3,16 +3,25 @@ import { Nav } from "reactstrap";
 import CVTitle from "./CVTitle";
 import AppContext from "../Context/AppContext";
 
-const PubTitles = props => (
+type Props = {
+  activeTab: number,
+  toggle: Function
+}
+
+type Tab = {
+  title: string
+}
+
+const PubTitles = (props: Props) => (
   <Nav tabs>
     <AppContext.Consumer>
       {context =>
-        context.PubTabTitles.map((tab, index) => (
+        context.PubTabTitles.map((tab: Tab, index: number) => (
           <CVTitle
             activeTab={props.activeTab}
             title={tab.title}
             number={index}
-            toggle={i => props.toggle(i)}
+            toggle={(i: number) => props.toggle(i)}
             key={index}
           />
         ))
