162a600ca9f62ae8e58bba0e19878c5e7b615116
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-03 17:28:13 +0200

    change CVTab to typescript

--- src/Components/CVTab.tsx
+++ src/Components/CVTab.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { TabPane } from "reactstrap";
 
-const CVTab = (props) => {
+type Props = {
+  data: {
+    list: {
+      time: string,
+      desc: string
+    }[]
+  },
+  number: number
+}
+
+const CVTab = (props: Props) => {
   const { list } = props.data;
   return (
     <TabPane tabId={props.number}>
-      <dl>
+      <dl> 
         {list.map((item, index) => (
           <React.Fragment key={index}>
             <dt>{item.time}</dt>
