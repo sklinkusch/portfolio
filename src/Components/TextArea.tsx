e15eab87419c3ae1670a924711da0707b3c76149
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-03 11:19:05 +0200

    change TextArea to typescript

--- src/Components/TextArea.tsx
+++ src/Components/TextArea.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-const TextArea = props => (
+type Props = {
+  id: string,
+  label: string,
+  name: string,
+  placeholder: string,
+  fieldRef: React.Ref<HTMLTextAreaElement>
+}
+
+const TextArea = (props: Props) => (
   <div className="form-group">
     <label htmlFor={props.id}>{props.label}</label>
     <textarea
       className="form-control"
       id={props.id}
-      rows="6"
+      rows={6}
       name={props.name}
       placeholder={props.placeholder}
       ref={props.fieldRef}
