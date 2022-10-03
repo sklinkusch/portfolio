125eae9adaf153d6ab3bfb47406915917a4543e4
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-03 17:34:06 +0200

    change CVTitles to typescript

--- src/Components/CVTitles.tsx
+++ src/Components/CVTitles.tsx
@@ -3,16 +3,25 @@ import CVTitle from "./CVTitle";
 import AppContext from "../Context/AppContext";
 import { Nav } from "reactstrap";
 
-const CVTitles = props => (
+type Props = {
+  activeTab: number,
+  toggle: Function
+}
+
+type Tab = {
+  title: string
+}
+
+const CVTitles = (props: Props) => (
   <Nav tabs>
     <AppContext.Consumer>
       {context =>
-        context.CVTabTitles.map((tab, index) => (
+        context.CVTabTitles.map((tab: Tab, index: number) => (
           <CVTitle
             activeTab={props.activeTab}
             title={tab.title}
             number={index}
-            toggle={i => props.toggle(i)}
+            toggle={(i: number) => props.toggle(i)}
             key={index}
           />
         ))
