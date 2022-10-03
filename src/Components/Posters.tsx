b682f18555a76727ad01132cf35d57e2a4530236
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-03 16:47:37 +0200

    change Posters to typescript

--- src/Components/Posters.tsx
+++ src/Components/Posters.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { TabPane } from "reactstrap";
 import AppContext from "../Context/AppContext";
 
+type Poster = {
+  authors: string,
+  title: HTMLQuoteElement,
+  event: HTMLSpanElement,
+  place: HTMLSpanElement,
+  year: HTMLSpanElement
+}
+
 const Posters = () => {
   const number = 2;
   const space = " ";
@@ -10,15 +18,17 @@ const Posters = () => {
       <ol reversed>
         <AppContext.Consumer>
           {context =>
-            context.posters.map((poster, index) => {
+            context.posters.map((poster: Poster, index: number) => {
               const { authors, title, event, place, year } = poster;
               return (
                 <li key={index}>
-                  <span className="authors">{authors}</span>,{space}
-                  {title},{space}
-                  {event},{space}
-                  {place},{space}
-                  {year}
+                  <>
+                    <span className="authors">{authors}</span>,{space}
+                    {title},{space}
+                    {event},{space}
+                    {place},{space}
+                    {year}
+                  </>
                 </li>
               );
             })
