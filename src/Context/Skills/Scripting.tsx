81a0f790133aa71353890d18ce6c3f1ac58f749b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-11-07 22:57:28 +0100

    adjust size of Bash logo

--- src/Context/Skills/Scripting.tsx
+++ src/Context/Skills/Scripting.tsx
@@ -1,52 +1,53 @@
-import javascript from "../../images/ES6LOGO.png";
+import javascript from "../../images/ES6LOGO.png"
 import typescript from "../../images/TypeScript.svg"
-import perl from "../../images/Perl.png";
-import bash from "../../images/Bash.png";
+import perl from "../../images/Perl.png"
+import bash from "../../images/Bash.png"
 
 const scripting = {
-  title: "Scripting Languages",
-  content: [
-    {
-      name: "ECMAScript 6",
-      imageSrc: javascript,
-      imageId: "ecmascript",
-      link: "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/",
-      imgStyles: {
-        width: "150px",
-      },
-    },
-    {
-      name: "TypeScript",
-      imageSrc: typescript,
-      imageId: "typescript",
-      link: "https://www.typescriptlang.org/",
-      imgStyles: {
-        width: "150px",
-      },
-    },
-    {
-      name: "Perl",
-      imageSrc: perl,
-      imageId: "perl",
-      link: "https://www.perl.org/",
-      imgStyles: {
-        minHeight: "210px",
-        maxWidth: "250px",
-        maxHeight: "250px",
-        width: "auto",
-      }
-    },
-    {
-      name: "Bourne-Again Shell",
-      imageSrc: bash,
-      imageId: "bash",
-      link: "https://www.gnu.org/software/bash/",
-      imgStyles: {
-        maxHeight: "200px",
-        width: "100%"
-      },
-    },
-  ],
-};
+	title: "Scripting Languages",
+	content: [
+		{
+			name: "ECMAScript 6",
+			imageSrc: javascript,
+			imageId: "ecmascript",
+			link: "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/",
+			imgStyles: {
+				width: "150px",
+			},
+		},
+		{
+			name: "TypeScript",
+			imageSrc: typescript,
+			imageId: "typescript",
+			link: "https://www.typescriptlang.org/",
+			imgStyles: {
+				width: "150px",
+			},
+		},
+		{
+			name: "Perl",
+			imageSrc: perl,
+			imageId: "perl",
+			link: "https://www.perl.org/",
+			imgStyles: {
+				minHeight: "210px",
+				maxWidth: "250px",
+				maxHeight: "250px",
+				width: "auto",
+			},
+		},
+		{
+			name: "Bourne-Again Shell",
+			imageSrc: bash,
+			imageId: "bash",
+			link: "https://www.gnu.org/software/bash/",
+			imgStyles: {
+				maxHeight: "200px",
+				maxWidth: "190px",
+				width: "100%",
+			},
+		},
+	],
+}
 
-export default scripting;
+export default scripting
