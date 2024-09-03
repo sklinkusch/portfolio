d9adbe77a0d75945958f54fdb6f6407f865a7a0f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-09-03 10:43:14 +0200

    order State Management skills alphabetically

--- src/Context/Skills/State.tsx
+++ src/Context/Skills/State.tsx
@@ -5,21 +5,21 @@ const state = {
   title: 'State Management',
   content: [
     {
-      name: 'Redux',
-      imageSrc: redux,
-      imageId: 'redux',
-      link: 'https://redux.js.org/',
-      imgStyles: {
-        width: '150px',
-      },
-    },
-    {
       name: 'Context API',
       imageSrc: react,
       imageId: 'react',
       link: 'https://reactjs.org/docs/context.html',
       imgStyles: {
         width: '250px',
+      },
+    },
+    {
+      name: 'Redux',
+      imageSrc: redux,
+      imageId: 'redux',
+      link: 'https://redux.js.org/',
+      imgStyles: {
+        width: '150px',
       },
     },
   ],
