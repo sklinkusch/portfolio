709495fc61af92b87ade512696b006afedebf878
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-09-03 11:19:19 +0200

    order Documentation skills alphabetically

--- src/Context/Skills/Documentation.tsx
+++ src/Context/Skills/Documentation.tsx
@@ -5,19 +5,19 @@ const documentation = {
   title: 'Documentation',
   content: [
     {
-      name: 'Storybook',
-      imageSrc: storybook,
-      imageId: 'storybook',
-      link: 'https://storybook.js.org',
+      name: 'Confluence',
+      imageSrc: confluence,
+      imageId: 'confluence',
+      link: 'http://www.atlassian.com/software/confluence/',
       imgStyles: {
         width: '180px',
       },
     },
     {
-      name: 'Confluence',
-      imageSrc: confluence,
-      imageId: 'confluence',
-      link: 'http://www.atlassian.com/software/confluence/',
+      name: 'Storybook',
+      imageSrc: storybook,
+      imageId: 'storybook',
+      link: 'https://storybook.js.org',
       imgStyles: {
         width: '180px',
       },
